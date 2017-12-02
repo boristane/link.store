52331e1f5189a4d12634036099ba41cd9cbd6504
Joël Tankam <joeltankam@student.emi.ac.ma>
2017-12-02 17:00:34 +0000

    Configure architecture

---
+++ server/app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+var app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Routes
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(err);
+});
+
+app.listen(PORT, function () {
+    console.log("Running in port " + PORT);
+});
